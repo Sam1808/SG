b4e6834265de48b80c4ec02bf0e869c8d63761ad
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2021-01-20 11:28:38 +0300

    Add spider_market app

---
+++ spider_project/spider_market/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SpiderMarketConfig(AppConfig):
+    name = 'spider_market'
--- spider_project/spider_project/settings.py
+++ spider_project/spider_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'spider_market.apps.Spider_marketConfig'
 ]
 
 MIDDLEWARE = [
