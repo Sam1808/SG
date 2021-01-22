a381b6ff372ea16ffc860a46bde5537e73b13549
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2021-01-22 14:39:03 +0300

    Add models to admin.py

--- spider_project/spider_market/admin.py
+++ spider_project/spider_market/admin.py
@@ -1,2 +1,26 @@
 from django.contrib import admin
 
+from .models import Category
+from .models import Company
+from .models import Product
+from .models import User
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
